b42c503c28baf7819a7dfaa3efb615db42669d96
Zhan Zhuridov <Zhanz@playtika.com>
2022-09-28 21:52:19 +0300

    fix line draw direction

--- src/tracer/tracelib/traceCore/tileLine.ts
+++ src/tracer/tracelib/traceCore/tileLine.ts
@@ -15,7 +15,9 @@ export function tileLine(start:Vector, finish:Vector, onPlot:(x:number, y: numbe
         y1 = finish.x;
     }
     // Если линия растёт не слева направо, то меняем начало и конец отрезка местами
+    /*let rev = false;
     if (x0 > x1){
+        rev = true
         let b = 0;
         b = x0;
         x0 = x1;
@@ -24,7 +26,7 @@ export function tileLine(start:Vector, finish:Vector, onPlot:(x:number, y: numbe
         b=y0;
         y0 = y1;
         y1 = b;
-    }
+    }*/
 
     let deltax = Math.abs(x1 - x0)
     let deltay = Math.abs(y1 - y0)
@@ -38,6 +40,7 @@ export function tileLine(start:Vector, finish:Vector, onPlot:(x:number, y: numbe
     if (diry < 0){
         diry = -1
     }
+    if (x0 < x1){
     for (let x= x0; x<= x1; x++){
         onPlot(steep ? y : x, steep ? x : y);
         error = error + deltaerr
@@ -46,5 +49,15 @@ export function tileLine(start:Vector, finish:Vector, onPlot:(x:number, y: numbe
             error = error - (deltax + 1)
         }
     }
+} else {
+    for (let x= x0; x >= x1; x--){
+        onPlot(steep ? y : x, steep ? x : y);
+        error = error + deltaerr
+        if (error >= (deltax + 1)){
+            y = y + diry
+            error = error - (deltax + 1)
+        }
+    } 
+}
     return x0-x1;
   }
